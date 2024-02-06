ea07e7e77a7a6632fd2a45ab8b10b06c900a2907
prakor <warunyoo084@gmail.com>
2024-02-06 14:23:23 +0700

    06/02/2024

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
 
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
 const concention = require("./config/db");
 const cors = require("cors");
 concention();
-
 
 app.use(bodyParser.json({ limit: "50mb", type: "application/json" }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,9 +13,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+//
+const prefix = "/Contract";
+
+// สัญญา มีที่ไม่มีทุน
+app.use(prefix + "/HaveplaceNocapital", require("./routes/contract2/index"));
+
 //test-sample
 app.get("/", (req, res) => {
-    res.send({ data: "hello world test   sdsdsadda 115zzz6\n5s5s5s5s5s5" })
+  res.send({ data: "hello world test   sdsdsadda 115zzz6\n5s5s5s5s5s5" });
 });
 
 //eContract
@@ -24,5 +29,5 @@ app.use("/contract", require("./routes/doc_a"));
 
 const port = process.env.PORT || 9987;
 app.listen(port, () => {
-    console.log(`\n--- test-eContract --- \nListening on port ${port}\n`)
-})
+  console.log(`\n--- test-eContract --- \nListening on port ${port}\n`);
+});
